8be62f45e08ee9a93be99f5300b234e7ba4c35b7
BlazsejKrisz <b.krisz4@gmail.com>
2024-10-31 19:16:52 +0100

    add todo component with checker

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import TodoList from "./components/TodoList"
 
 const App = () => {
   return (
     <main>
       <Header/>
+      <TodoList/>
     </main>
   )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,50 @@
-const TodoList = () => {
+import { useEffect, useState } from "react";
+
+interface ToDo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const TodoList: React.FC = () => {
+  const toDoList: ToDo[] = [
+    {
+      id: 1,
+      title: "Do the dishes",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Do the laundry",
+      completed: true,
+    },
+  ];
+  const [toDos, setToDos] = useState<ToDo[]>(toDoList);
+  const [title, setTitle] = useState<string>("");
+
+  const handleChange = (id: any) => {
+    setToDos((prevToDos) =>
+      prevToDos.map((toDo) =>
+        toDo.id === id ? { ...toDo, completed: !toDo.completed } : toDo
+      )
+      
+    );
+  };
+
+useEffect(() => {
+  console.log(toDos)
+},[toDos])
 
   return (
     <div>
-
+      {toDos.map((toDo) => (
+        <div key={toDo.id}>
+          {toDo.title}
+          <input type="checkbox" checked={toDo.completed} onChange={() => handleChange(toDo.id)} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default TodoList
+export default TodoList;
